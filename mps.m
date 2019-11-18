543e428c76a5371d963a4333edb87e48a56f162c
Lucas Soares Pereira <lucas_pereira@tpn2.usp.br>
2019-11-18 18:05:17 -0200

    Adding calculation of lambda

--- mps.m
+++ mps.m
@@ -1,5 +1,6 @@
-folder = "C:\Users\jefferson_oliveira\Documents\MPS\";
+folder = "C:\Users\lucas_pereira.TPN2\Documents\mps\";
 file = "2D_dam_1512.grid";
 fileName = strcat(folder, file);
 
 [npart, grid] = readGrid(fileName);
+[lambda]=calcLambda(0.01,4);
